f8324beb733e82c000e16e81f24cbb2713889a4b
yangfriendship <yangfriendship@naver.com>
2021-04-03 00:02:35 +0900

    create entity

--- src/main/java/com/security/jwt/domain/Account.java
+++ src/main/java/com/security/jwt/domain/Account.java
@@ -1,4 +1,29 @@
 package com.security.jwt.domain;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
 public class Account {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "account_id")
+    private Long id;
+
+    private String email;
+
+    private String password;
+
+    private Role role;
+
 }
---
+++ src/main/java/com/security/jwt/domain/Role.java
@@ -0,0 +1,17 @@
+package com.security.jwt.domain;
+
+public enum Role {
+
+    USER("ROLE_USER"),MANAGER("ROLE_MANAGER"),ADMIN("ROLE_ADMIN");
+
+    private final String code;
+
+    Role(String code) {
+        this.code = code;
+    }
+
+    @Override
+    public String toString() {
+        return this.code;
+    }
+}
